d9a7c46295efab271d9ed22aed173db3dca90a76
falkTX <falktx@gmail.com>
2013-12-19 00:11:28 +0000

    More cleanup

--- inc_footer.php
+++ inc_footer.php
@@ -1,15 +1,12 @@
 <footer>
-		
   <div class="container">
    <div class="wrapper">
-		
-		<div style="position:relative; top: 0px;left: 0px;">
-		  <form class="formleftside" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>" method="post">
-			<input type="submit" name="changeColorNow" value="Switch color-theme"/>
-		  </form>
-		</div>
-  
-		<div><p style="font-size: 90%;">&#0169; 2012 DISTRHO&nbsp; &nbsp;<span style="float:right;"> Sponsored by <a href="http://kxstudio.sf.net" target="_blank">KXStudio</a>&nbsp; &nbsp;|&nbsp; &nbsp;All the plugins and linux ports here are free, please consider a <a href="http://sourceforge.net/donate/index.php?group_id=539284" target="_blank">donation</a> if possible.</p></span></div>
+      <div style="position:relative; top: 0px;left: 0px;">
+        <form class="formleftside" action="<?php echo $_server['script_name']; ?>" method="post">
+          <input type="submit" name="changecolornow" value="switch color-theme"/>
+        </form>
+      </div>
+      <div><p style="font-size: 90%;">&#0169; 2012 DISTRHO&nbsp; &nbsp;<span style="float:right;"> Sponsored by <a href="http://kxstudio.sf.net" target="_blank">KXStudio</a>&nbsp; &nbsp;|&nbsp; &nbsp;All the plugins and linux ports here are free, please consider a <a href="http://sourceforge.net/donate/index.php?group_id=539284" target="_blank">donation</a> if possible.</p></span></div>
     </div>
   </div>
 </footer>
