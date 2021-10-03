c5197a86b5637fd0920cb03fc1d221b0d0d57a01
falkTX <falktx@falktx.com>
2021-10-03 13:06:51 +0100

    Start the rework
    Signed-off-by: falkTX <falktx@falktx.com>

--- inc_footer.php
+++ inc_footer.php
@@ -1,28 +1,21 @@
 <footer>
-  <div class="container">
-   <div class="wrapper">
-      <div style="position:relative; top: 0px;left: 0px;">
-        <form class="formleftside" action="<?php echo $_server["script_name"]; ?>" method="post">
-          <input type="submit" name="changeColorNow" value="Switch Color Theme"/>
-        </form>
-      </div>
-      <div>
-        <p style="font-size: 90%;">
-          &#0169;
-          2012-2018 DISTRHO
-          &nbsp; &nbsp;
-          <span style="float:right;">
-            Sponsored by
-              <a href="https://kxstudio.linuxaudio.org" target="_blank">KXStudio</a>
-            &nbsp; &nbsp;|&nbsp; &nbsp;
-            All the plugins and Linux ports here are free, please consider a
-              <a href="https://kxstudio.linuxaudio.org/Donations" target="_blank">donation</a>
-            if possible.
-          </span>
-        </p>
-      </div>
+    <div class="container">
+        <div class="wrapper">
+            <p>
+                &#0169;
+                2012-2021 DISTRHO
+                &nbsp; &nbsp;
+                <span style="float:right;">
+                    Hosted and maintained by
+                        <a href="https://kx.studio/" target="_blank">KXStudio</a>
+                    &nbsp; &nbsp;|&nbsp; &nbsp;
+                    All the plugins and Linux/LV2 ports here are free and open-source, please consider a
+                        <a href="https://kx.studio/Donations" target="_blank">donation</a>
+                    if possible.
+                </span>
+            </p>
+        </div>
     </div>
-  </div>
 </footer>
 </body>
 </html>
--- inc_header.php
+++ inc_header.php
@@ -1,19 +1,5 @@
 <?php
 
-function changeColor() {
-    if ($GLOBALS["THEME"] == "white") {
-        $GLOBALS["THEME"] = "black";
-    } else {
-        $GLOBALS["THEME"] = "white";
-    }
-
-    setcookie("color-theme", $GLOBALS["THEME"], time() + (365 * 24 * 60 * 60)); // expires in 1 year
-
-    // force page reload
-    header("location: " . $_server["script_name"]);
-    exit();
-}
-
 if (file_exists("/home/falktx/Personal/FOSS/")) {
     $ROOT = "/distrho";
 } else {
@@ -24,10 +10,6 @@ if (isset($_COOKIE["color-theme"])) {
     $GLOBALS["THEME"] = htmlspecialchars($_COOKIE["color-theme"]);
 } else {
     $GLOBALS["THEME"] = "white";
-}
-
-if (isset($_POST["changeColorNow"])) {
-    changeColor();
 }
 
 $VERSION_DPF   = "v1.1";
@@ -47,55 +29,7 @@ $URL_PORTS = "https://github.com/DISTRHO/DISTRHO-Ports/releases/download/" . $VE
 <?php if ($CURRENT_PAGE == "index") { ?>
   <link rel="stylesheet" href="<?php echo $ROOT; ?>/css-<?php echo $GLOBALS["THEME"]; ?>/style-mini-news.css" type="text/css" media="all">
 <?php } ?>
-  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Raleway:200,300' type='text/css' media="all">
-  <script type="text/javascript" src="<?php echo $ROOT; ?>/js/jquery-1.4.2.min.js" ></script>
-<?php if ($CURRENT_PAGE == "index") { ?>
-  <script type="text/javascript" src="<?php echo $ROOT; ?>/js/roundabout.js"></script>
-  <script type="text/javascript" src="<?php echo $ROOT; ?>/js/roundabout_shapes.js"></script>
-  <script type="text/javascript" src="<?php echo $ROOT; ?>/js/gallery_init.js"></script>
-  <script type="text/javascript" src="<?php echo $ROOT; ?>/js/responsiveslides.min.js"></script>
-<?php } else if ($CURRENT_PAGE == "plugins" || $CURRENT_PAGE == "ports") { ?>
-  <script type="text/javascript" src="<?php echo $ROOT; ?>/js/prototype.js"></script>
-  <script type="text/javascript" src="<?php echo $ROOT; ?>/js/lightbox.js"></script>
-  <script type="text/javascript" src="<?php echo $ROOT; ?>/js/scriptaculous.js?load=effects,builder"></script>
-  <script type="text/javascript">
-<?php
-    if ($CURRENT_PAGE == "plugins") {
-        $menuClasses = array("miniseries", "nekobi", "kars", "glbars", "prom");
-    } else if ($CURRENT_PAGE == "ports") {
-        $menuClasses = array("arctican", "dexed", "drowaudio", "easyssp", "juceopl", "juced", "klangfalter",
-                             "lufsmeter", "luftikus", "mverb", "ndc", "obxd", "pitcheddelay", "refine", "tal",
-                             "stereosourceseparator", "vex", "wolpertinger");
-    } else {
-        $menuClasses = array();
-    }
-?>
-    jQuery(document).ready(function() {
-<?php for ($i = 0; $i < count($menuClasses); $i++) { ?>
-        jQuery('img.menu_class_<?php echo $menuClasses[$i]; ?>').click(function() {
-            jQuery('ul.the_menu_<?php echo $menuClasses[$i]; ?>').slideToggle('fast');
-        });
-<?php } ?>
-    });
-  </script>
-<?php } ?>
-  <script type="text/javascript">
-    $(function() {
-      // Slideshow 1
-      $(".rslides").responsiveSlides({
-          nav: false,
-          pager: true,
-          namespace: "centered-btns"
-      });
-    });
-  </script>
-  <!--[if lt IE 7]>
-    <link rel="stylesheet" href="<?php echo $ROOT; ?>/css/ie/ie6.css" type="text/css" media="all">
-  <![endif]-->
-  <!--[if lt IE 9]>
-    <script type="text/javascript" src="<?php echo $ROOT; ?>/js/html5.js"></script>
-    <script type="text/javascript" src="<?php echo $ROOT; ?>/js/IE9.js"></script>
-  <![endif]-->
+  <link rel='stylesheet' href='<?php echo $ROOT; ?>/fonts/fonts.css' type='text/css' media="all">
 </head>
 <body>
 <header>
